34895e69f2c2d9af8d163ea166eec2af0bda4e9f
Anton Villadolid <antonmiguelvilladolid@gmail.com>
2021-01-10 17:45:52 -0800

    Added .env file to read support variables

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: process.env.postgresUser,
     password: process.env.postgresPassword,
-    host: process.env.postgreshost,
+    host: process.env.postgresHost,
     port: process.env.postgresPort,
     database: process.env.postgresDatabase
 })
